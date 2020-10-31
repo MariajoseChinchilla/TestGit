5da81fa901492106cd1e2c03394f3178924cde9c
MariajoseChinchilla <mchinchimoran@gmail.com>
2020-10-31 12:25:40 -0600

    hola

--- Main.py
+++ Main.py
@@ -16,22 +16,21 @@ import time
 
 
 class JuegoDeLaVida(Gtk.Window):
-    global vbox
-    vbox = Gtk.VBox()
-    global grid
-    grid = Gtk.Grid()
-    vbox.pack_start(grid, True, True, 0)
-    grid.set_row_spacing(2)  # separacion entre filas
-    grid.set_column_spacing(2)  # separacion entre columnas
+    cuenta = 0
     def __init__(self):
         super(JuegoDeLaVida, self).__init__(title='Juego de la vida')
     #Cuestiones basicas para la ventana y los contenedores
         self.set_default_size(800, 600)
         self.set_resizable(False)  # fijar el tamaño de la ventana
+        vbox = Gtk.VBox()
         self.add(vbox)
-    #cuestiones relacionadas a lo que servira para el intervalo de espera y guardar archivo con fecha
-        pausa = False
-        momento = datetime.now()
+        grid = Gtk.Grid()
+        grid.set_row_spacing(2)
+        grid.set_column_spacing(2)
+        vbox.pack_start(grid, True, True, 0)
+        self.pause = True
+        self.now = datetime.now()
+
 
     #Menu y todos sus items
         menubar = Gtk.MenuBar()  # barra de menu
@@ -73,14 +72,9 @@ class JuegoDeLaVida(Gtk.Window):
         menubar.append(ayuda)
         grid.attach(menubar, 0,0, 5, 1)
 
-        buffer = Gtk.TextBuffer()
-        self.display = Gtk.TextView(buffer=buffer)
-        self.display.set_size_request(30, 30)
-        grid.attach(self.display, 10, 0, 1, 1)
-
         #tiempo = Gtk.SpinButton()
         #self.TimeFrame = tiempo.get_value_as_int()
-        self.TimeFrame = 0.1
+        self.TimeFrame = 0.3
 
 #el submenu de configuracion aparece siempre y no esta en la barra de menu para facilidad de modificacion de
         #las configuraciones
@@ -105,6 +99,14 @@ class JuegoDeLaVida(Gtk.Window):
         ajuste = Gtk.Adjustment(lower=0.00001, upper=5, step_increment=0.01, page_increment=0.01)
         segundos.set_adjustment(ajuste)
         grid.attach_next_to(segundos, segundos_label, Gtk.PositionType.BOTTOM, 1, 1)
+
+#botones de pausar/jugar y el de tomar captura de pantalla
+        pausa_jugar_boton = Gtk.Button('Jugar/Pausar')
+        pausa_jugar_boton.connect('clicked', self.play_pause)
+        grid.attach(pausa_jugar_boton, 24, 200, 1, 1)
+        captura_boton = Gtk.Button('Captura de pantalla')
+        captura_boton.connect('clicked', self.captura_de_pantalla)
+        grid.attach_next_to(captura_boton, pausa_jugar_boton, Gtk.PositionType.BOTTOM, 2, 2)
 
     #metodos para cada boton
 
@@ -142,17 +144,26 @@ class JuegoDeLaVida(Gtk.Window):
             for x in range(N):
                 tablero[y, x] = random.randint(0, 1)
 
+# funciones para cargar un archivo inicial y poner pausa y demas
+
+    def play_pause(self, widget):
+        self.pause ^= True      #esto va a servir como un interruptor para normal_cargado funcion
+
+    def captura_de_pantalla(click):
+        print('captura boton')
+
 # para evaluar que debe pasar con una celula, evaluar a sus 8 vecinas
 #considerar "mover" las celulas en las 8 direcciones. Por cada movimiento, sumar el
 #valor de la celula en esa posicion. El valor final que tome cada celula luego de los 8
 #movimientos es el numero de celulas vecinas vivas.
     def normales_cargado(self, tablero):
+
         def conteo_normales(tablero):
             global vecindario
-            vecindario = np.zeros(len(tablero), len(tablero))
+            vecindario = np.zeros((len(tablero), len(tablero)), dtype = int)
             for j in range(1, len(tablero) - 1):
                 for i in range(1, len(tablero) - 1):
-                    tablero[j, i] = (
+                    vecindario[j, i] = (
                             tablero[j + 1, i - 1] +  # Abajo - Izquierda
                             tablero[j + 1, i] +  # Abajo
                             tablero[j + 1, i + 1] +  # Abajo - Derecha
@@ -162,98 +173,106 @@ class JuegoDeLaVida(Gtk.Window):
                             tablero[j - 1, i - 1] +  # Arriba - Izquierda
                             tablero[j, i - 1]  # Izquierda
                     )
-                for i in range(1, len(tablero) - 1):
-                    tablero[0, i] = (
+            for i in range(1, len(tablero) - 1):
+                    vecindario[0, i] = (
                             tablero[0, i - 1] +  # Izquierda
                             tablero[1, i - 1] +  # Abajo - Izquierda
                             tablero[1, i] +  # Abajo
                             tablero[1, i + 1] +  # Abajo - Derecha
                             tablero[0, i + 1]  # Derecha
                     )
-                for i in range(1, len(tablero) - 1):
-                    tablero[N - 1, i] = (
+            for i in range(1, len(tablero) - 1):
+                    vecindario[N - 1, i] = (
                             tablero[N - 1, i - 1] +  # Izquierda
                             tablero[N - 2, i - 1] +  # Arriba - Izquierda
                             tablero[N - 2, i] +  # Arriba
                             tablero[N - 2, i + 1] +  # Arriba - Derecha
                             tablero[N - 1, i + 1]  # Derecha
                     )
-                for j in range(1, len(tablero) - 1):
-                    tablero[j, 0] = (
+            for j in range(1, len(tablero) - 1):
+                    vecindario[j, 0] = (
                             tablero[j - 1, 0] +  # Arriba
                             tablero[j - 1, 1] +  # Arriba - Derecha
                             tablero[j, 1] +  # Derecha
                             tablero[j + 1, 1] +  # Abajo - Derecha
                             tablero[j + 1, 0]  # Abajo
                     )
-                for j in range(1, len(tablero) - 1):
-                    tablero[j, 0] = (
+            for j in range(1, len(tablero) - 1):
+                    vecindario[j, 0] = (
                             tablero[j - 1, N - 1] +  # Arriba
                             tablero[j - 1, N - 2] +  # Arriba - Izquierda
                             tablero[j, N - 2] +  # Izquierda
                             tablero[j + 1, N - 2] +  # Abajo - Izquierda
                             tablero[j + 1, N - 1]  # Abajo
                     )
-                tablero[0, 0] = (
-                        tablero[0, 1] +  # Derecha
-                        tablero[1, 1] +  # Abajo - Derecha
-                        tablero[1, 0]  # Abajo
-                )
-                tablero[0, N - 1] = (
-                        tablero[0, N - 2] +  # Izquierda
-                        tablero[1, N - 2] +  # Abajo - Izquierda
-                        tablero[1, N - 1]  # Abajo
-                )
-                tablero[N - 1, 0] = (
-                        tablero[N - 2, 0] +  # Arriba
-                        tablero[N - 2, 1] +  # Arriba - Derecha
-                        tablero[N - 1, 1]  # Derecha
-                )
-                tablero[N - 1, N - 1] = (
-                        tablero[N - 2, N - 1] +  # Arriba
-                        tablero[N - 2, N - 2] +  # Arriba - Izquierda
-                        tablero[N - 1, N - 2]  # Izquierda
-                )
-                return vecindario
+            vecindario[0, 0] = (
+                    tablero[0, 1] +  # Derecha
+                    tablero[1, 1] +  # Abajo - Derecha
+                    tablero[1, 0]  # Abajo
+            )
+            vecindario[0, N - 1] = (
+                    tablero[0, N - 2] +  # Izquierda
+                    tablero[1, N - 2] +  # Abajo - Izquierda
+                    tablero[1, N - 1]  # Abajo
+            )
+            vecindario[N - 1, 0] = (
+                    tablero[N - 2, 0] +  # Arriba
+                    tablero[N - 2, 1] +  # Arriba - Derecha
+                    tablero[N - 1, 1]  # Derecha
+            )
+            vecindario[N - 1, N - 1] = (
+                    tablero[N - 2, N - 1] +  # Arriba
+                    tablero[N - 2, N - 2] +  # Arriba - Izquierda
+                    tablero[N - 1, N - 2]  # Izquierda
+            )
+            return vecindario
+
         def paso(tablero):
         # evaluacion de que pasara con la celula dependiendo de las reglas del juego
             v = conteo_normales(tablero)
             nuevo_tablero = tablero.copy()
+            clase = JuegoDeLaVida
+            contador = clase.cuenta
             for i in range(nuevo_tablero.shape[0]):
                 for j in range(nuevo_tablero.shape[1]):
                     if v[i, j] == 3 or (v[i, j] == 2 and tablero[i, j]):
                         nuevo_tablero[i, j] = 1
+                        contador = contador + 1
                     else:
                         nuevo_tablero[i, j] = 0
+                        contador = contador + 1
             return nuevo_tablero
-        def animacion(i):
-            global tablero
-            if self.pause == False:
-                self.buffer.set_text(str(i))
-                estado = paso(tablero)
-                imagen.set_data(estado)
 
-            return imagen,
-# Parte grafica del tablero del juego
+#creacion de figura en matplotlib
+
         fig = plt.figure(figsize=(6, 6))
         ax = fig.add_subplot(111)
-        global imagen
+        titulo = f'Turno: {self.cuenta}'  # poner el numero de turno como titulo de la figura de matplotlib
+        ax.set_title(titulo)  # agrega lo anterior como titulo de la figura
         imagen = ax.imshow(tablero, interpolation="none", aspect="equal", cmap=cm.bwr)
         plt.tick_params(
             axis='x',
             which='both',
             bottom=False,
             top=False,
-            labelleft=False,
             labelbottom=False)
 
-        anim = animation.FuncAnimation(fig, animacion, frames=100, blit=True, interval=(self.TimeFrame * 1000),
-                                           repeat=True)
-        #agregar lo de matplot lib a la ventana del Gtk
+        def animacion(i):
+            global tablero
+            if self.pause == False:
+                tablero = paso(tablero)
+                imagen.set_data(tablero)
 
-        return FigureCanvas(fig)
+            return imagen,
 
+        anim = animation.FuncAnimation(fig, animacion,
+                                       frames=100, blit=True, interval=(self.TimeFrame * 1000),
+                                       repeat=True)
+        plt.show()
 
+#sospecho que se esta agregando al scrolled window pero este no esta agregado en la ventana principal
+
+        #mostrar figura
     def fronteras_toroidales(self, widget):
         def vecindad(tablero):
 #conteo de las vivas para ver si una celula tiene sobrepoblacion o esta en soledad
@@ -283,35 +302,44 @@ class JuegoDeLaVida(Gtk.Window):
                         nuevo_tablero[i, j] = 0
             return nuevo_tablero
 
-#parte grafica del tablero,esto luego se agrega usado un scrolled window y FigCanvas
-        fig = plt.figure(figsize=(6, 6))
-        ax = fig.add_subplot(111)
-        imagen = ax.imshow(tablero, interpolation='none', aspect = 'equal', cmap=cm.bwr)
-
-        plt.tick_params(
-            axis='x',
-            which='both',
-            bottom=False,
-            top=False,
-            labelleft = False,
-            labelbottom=True)
-
         def animacion(i):
             global tablero
             if self.pause == False:
-                self.buffer.set_text(str(i))
                 tablero = paso(tablero)
                 imagen.set_data(tablero)
 
             return imagen,
 
-        anim = animation.FuncAnimation(fig, animacion, frames=100, blit=True,
-                                       interval = (self.timeFrame * 1000), repeat = True)
+#parte grafica del tablero,esto luego se agrega usado un scrolled window y FigCanvas
+        fig = plt.figure(figsize=(6, 6))
+        ax = fig.add_subplot(111)
+        titulo = f'Turno: {contador}'  # poner el numero de turno como titulo de la figura de matplotlib
+        ax.set_title(titulo)  # agrega lo anterior como titulo de la figura
+        global imagen
+        imagen = ax.imshow(tablero, interpolation="none", aspect="equal", cmap=cm.bwr)
+        plt.tick_params(
+            axis='x',
+            which='both',
+            bottom=False,
+            top=False,
+            labelleft=False,
+            labelbottom=False)
 
-        scrolled = Gtk.ScrolledWindow()
-        canvas = FigureCanvas(fig)
-        scrolled.add(canvas)
-        grid.attach(scrolled, 3, 3, 30, 30)
+        anim = animation.FuncAnimation(fig, animacion, frames=100, blit=True, interval=(self.TimeFrame * 1000),
+                                       save_count=1, repeat=True)
+        plt.show()
+
+        # botones de play/pause y de generar captura de pantalla
+        boton_jugar = Gtk.Button('Jugar/Pausa')
+        boton_jugar.connect('clicked', )
+        grid.attach_next_to(boton_jugar, scrolled, Gtk.PositionType.RIGHT, 5, 5)
+
+        boton_captura = Gtk.Button('Captura de pantalla')
+        boton_captura.connect('clicked', self.captura_de_pantalla)
+        grid.attach_next_to(boton_captura, boton_jugar, Gtk.PositionType.RIGHT, 5, 5)
+
+        t = FigureCanvas(fig)
+        scrolled.add(t)
 
     def segundos_espera(self,widget):
         pass
@@ -325,15 +353,14 @@ class JuegoDeLaVida(Gtk.Window):
         acerca_de_dialogo.set_authors('MCM')
         acerca_de_dialogo.set_copyright('Desarrollo de interfaz gráfica en Gtk 3.0')
         acerca_de_dialogo.set_comments('Uso de Gtk 3.0, Numpy, MatplotLib')
-        acerca_de_dialogo.set_website('https://developer.gnome.org/gtk3/stable/GtkMenuItem.html')
+        acerca_de_dialogo.set_website('https://github.com/MariajoseChinchilla/TestGit/blob/master/Main.py')
         vbox.pack_start(acerca_de_dialogo, False, False, 0)
         self.add(vbox)
         acerca_de_dialogo.run()
         acerca_de_dialogo.destroy()
 
-    def codigo_fuente(self, widget):        #no olvidar cambiar esto cuando ya lo haya subido a GitHub
-        webbrowser.open_new_tab('https://python-gtk-3-tutorial.readthedocs.io/en/latest/button_widgets.html?highlight=button')
-
+    def codigo_fuente(self, widget):
+        webbrowser.open_new_tab('https://github.com/MariajoseChinchilla/TestGit/blob/master/Main.py')
 
 
 #Acciones de los radio botones
